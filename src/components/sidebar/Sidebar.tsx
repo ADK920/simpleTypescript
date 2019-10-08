e24714a8f34066f70244e843367857aae89bad45
Andrew <andrew.karnis@gmail.com>
2019-10-07 20:43:28 -0400

    handle new report, change title

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ type SidebarProps = {
 
 export const Sidebar: FunctionComponent<SidebarProps> = (props: SidebarProps) => {
     const blankReport: ReportConfig = { title: '', content: '' };
-    const [newReport, setReport] = useState(blankReport);
+    const [newReport, setReport] = useState({...blankReport});
 
     const updateNewReport = (e: any) => {
         const report: ReportConfig = { ...newReport };
@@ -18,12 +18,17 @@ export const Sidebar: FunctionComponent<SidebarProps> = (props: SidebarProps) =>
         setReport(report);
     }
 
+    const handleAddReport = () => {
+        props.handleNewReport(newReport);
+        setReport({...blankReport});
+    }
+
     return (
         <div className="sidebar">
             <h5>Reports</h5>
             <div>
                 <input type="text" value={newReport.title} onChange={(e) => updateNewReport(e)}></input>
-                <button onClick={() => props.handleNewReport(newReport)}>Add Report</button>
+                <button onClick={() => handleAddReport()}>Add Report</button>
             </div>
             {props.reports.map((report: ReportConfig, index: number) => {
                 return <div className='report-item' key={index} onClick={()=>props.handleReportSelection(report)}>{report.title}</div>
