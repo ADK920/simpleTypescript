aa0703aa55588ca016d1acd8ca9c8f2403c878b3
Andrew <andrew.karnis@gmail.com>
2019-10-09 14:10:34 -0400

    allow edit

--- src/components/report/Report.tsx
+++ src/components/report/Report.tsx
@@ -4,6 +4,7 @@ import { ReportConfig } from '../../types';
 type ReportProps = {
     reports: ReportConfig[];
     report: ReportConfig;
+    handleReportEdit: Function;
 }
 
 export const Report: FunctionComponent<ReportProps> = (props: ReportProps) => {
@@ -11,8 +12,22 @@ export const Report: FunctionComponent<ReportProps> = (props: ReportProps) => {
         'edit': 'save',
         'save': 'edit'
     };
+    const report = { ...props.report };
     const [mode, setMode] = useState<string>('save');
-    const editor = props.report ? (<button onClick={() => setMode(modes[mode])}>{modes[mode]}</button>) : '';
+    const [content, setContent] = useState<string>(report.content || '');
+
+    const handleSetMode = () => {
+        const newMode = modes[mode];
+        if (newMode === 'edit') {
+            setContent(report.content);
+        } else {
+            const editedReport: ReportConfig = { ...report };
+            editedReport.content = content;
+            props.handleReportEdit(editedReport);
+        }
+        setMode(modes[mode]);
+    }
+    const editor = props.report ? (<button onClick={handleSetMode}>{modes[mode]}</button>) : '';
     const header = <div className='report-header'>
         <div>
             <h5>{props.report ? `Selected: ${props.report.title}` : 'No Report Selected'}</h5>
@@ -20,7 +35,13 @@ export const Report: FunctionComponent<ReportProps> = (props: ReportProps) => {
         </div>
     </div>;
     const editContent = (<div>
-        <textarea name="reportContent" placeholder="Edit the report content here" cols={30} rows={10}></textarea>
+        <textarea
+            name="reportContent"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Edit the report content here"
+            cols={30}
+            rows={10}></textarea>
     </div>);
     const displayContent = (<div>
         {props.report ? props.report.content : ''}
